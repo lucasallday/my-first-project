d8c582023bf8cab73f88699c8d13da82ddd2a9b6
imears <maxwellmears@gmail.com>
2017-03-04 00:57:24 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var button = document.getElementById('button');
+
+  button.addEventListener("click", function() {
+    alert('Do not open')
+  });
+});
